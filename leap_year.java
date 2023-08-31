91a726069ea662750a58062957a7c19ea7f6f1c6
Harshal sonwane <112231198+Hrshalhack@users.noreply.github.com>
2023-08-31 10:43:13 +0530

    Add files via upload

---
+++ leap_year.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class leap_year{
+
+
+
+
+    public static void main(String args[]){
+
+    int year = 9;
+
+    if(year%4==0){
+        System.out.print("yes");
+    }
+    else{
+          System.out.print("No");
+    }
+
+
+
+
+
+
+
+    }
+}
+
+
