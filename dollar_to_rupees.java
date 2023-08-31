23bcecaa12ebbdc42bb770109737738e81da6617
Harshal sonwane <112231198+Hrshalhack@users.noreply.github.com>
2023-08-31 10:50:19 +0530

    Add files via upload

---
+++ dollar_to_rupees.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+class dollar_to_rupees{
+
+
+
+
+    public static void main(String args[]){
+
+    float dollar = 9;
+    
+
+    
+        System.out.print(dollar*82.65);
+   
+    }
+}
+
